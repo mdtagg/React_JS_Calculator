cf3a2da13ffca5af6011addfc6a945147744be6a
Mike Tagg <mdtagg91@gmail.com>
2023-01-04 12:35:00 -0800

    added buttons and output, styling added
    Buttons and output were added the challenge currently is that i want the calculator to be a
    seperate div standing alone. Originally in the old design there was not background that had to
    stand out from the body. I am going to separate the output from the buttons into to seperate divs
    and style them accordingly.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,33 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      test
+    <>
+    <div className='calculator-container'>
+      <div className='output'>
+        <div className='operand'>1</div>
+      </div>
+      <button>AC</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button className='span-two'>0</button>
+      <button>.</button>
+      <button>=</button>
     </div>
+    </>
+    
   )
 }
 
